726848a83e194ae4d4ccd80460e7f532b9131345
Rebecca Fritz <86808098+RebeccaFritz@users.noreply.github.com>
2024-11-18 10:07:31 -0600

    Update ParticleCollisionBinaryStars.py
    Switching to Web Vpython, adding constants and values from the sample code, adding a function to create particles

--- ParticleCollisionBinaryStars.py
+++ ParticleCollisionBinaryStars.py
@@ -1,56 +1,73 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 13 14:45:08 2024
+Web VPython 3.2
+from random import randrange
 
-@author: rgfri
-"""
-
-import vpython as vp
-
-autoscale = 1
+# create canvases
+scene.autoscale = 1
+scene = canvas(title = 'Particle Collisions in a Binary Star System', color = color.black, align='left')
 
 # Define Globals
 G  = 6.67E-11 # gravitational constant
-P = ... # orbital period
-AMBIENT_COLOR # color of particles when ambient temp 
-""" Eventually:
-HOT_COLOR # color of particles when hot
-COLD_COLOR # color of particles when cold
-"""
-h = # time step
-L1 # distance
+AU = 1.5E11 # astronomical unit
+YEAR = 365.25*24*60*60 # year in seconds
+MS = 1.988400e20 # mass of the sun 
+P = 7.8719E3 # orbital period                  # from sample code
+MID_COLOR = vec(2.50, 1.50, 0.50) # color of particles when ambient temp 
+HIGH_COLOR = vec(2.00, 0.00, 0.00) # color of particles when hot
+LOW_COLOR = vec(2.50, 2.00, 0.00) # color of particles when cold
+STARA_COLOR = vec(2.55, 2.50, 2.00)
+STARB_COLOR = vec(2.55, 2.55, 2.55)
 
-# create Objects
-starA """ If we don't want to animate the stars we can just put their mass and position and such in variables"""
-starB
-# legend """ Eventually """
-starA.trail """ do we want trails"""
-starB.trail
-accretionDisk = """ do we still want to do this? could do two transparent cylinders compounded together"""
+h = 2.0 # time step                            # from sample code
+L1 = 3.69E8 # distance to Lagrange Point 1            # from sample code
+
+# create scene objects
+starA = sphere(pos=vec(0,0,0), radius= 6.6E6, mass=1.5E30, color=vec(0,1,0), visible=True) # numbers from sample code
+starB = sphere(pos=vec(5.80E8,0,0), radius= 1.5E8, mass=3.75E29, color=vec(0,0.5,0), visible=True) # numbers from sample code
+# legend = # Eventually 
+starA.trail = curve(pos=starA.pos, color=starA.color) # do we want trails
+starB.trail = curve(pos=starB.pos, color=starB.color)
+# accretionDisk = # do we still want to do this? could do two transparent cylinders compounded together
 
 
 # a function to create particles
 ## vector, vector --> sphere
-""" I already created this """
+def create_particle(pos, vel):
+    return sphere(pos = pos, vel = vel, mass = 2.0e10, radius = 4E6, color = MID_COLOR) # numbers from sample code
 
 # ancillaries
-particles = [] # list of particles
 t = 0 # time
+
+#create 1000 particles and add them to a list
+particles = []
+for i in range(0, 1000):
+    x = randrange(0,100)*4E6 # pick random number                        # need to update these
+    y = randrange(0,100)*4E6
+    z = randrange(0,100)*4E6
+    vx = 0                                                               # need to update these 
+    vy = 0
+    vz = 0
+    particles.append(create_particle(vec(x,y,z), vec(vx,vy,vz)))
 
 # a function to determine the acceleration of a star
 ## ... --> ...
+"""
 def accStar(...):
     return
+"""
 
 # a function to run the Runge-Kutta alg on a star
 ## ... --> ...
+"""
 def rkStar(...):
     return
+"""
 
 # a function to check if the particles have collided
 ## ... --> ...
+"""
 def particleCol(...):
     return
+"""
 
 # a function to run the Runge-Kutta alg on particles
 """
@@ -59,8 +76,10 @@ single particle here and loop in the animation we can do
 it whichever way we prefer
 """
 ## ... --> ...
+"""
 def rkParticle(...):
     return
+"""
 
 """ Eventually:
 # a function to check particle temperature and change their color
@@ -71,4 +90,4 @@ def particleTemp(...):
 
 # run animation
 while True:
-    rate(...)
+    rate(30) # allows the program to look the same on every computer
